a77b505a0ff4cf46a3fe8ee71488d77334a51334
Jasper <jasperdijk27@gmail.com>
2019-11-25 19:28:13 +0100

    first commit

---
+++ users.php
@@ -0,0 +1,45 @@
+<?php
+$name = $_POST["name"];
+$myObj = new \stdClass();
+ if(isset($_POST['checkYes'])){
+
+     $current_data = file_get_contents('results.json');
+     $array_data = json_decode($current_data, true);
+     $extra = array(
+         'Name'               =>     $_POST['name'],
+         'Value'          =>     "Yes"
+     );
+     $array_data[] = $extra;
+     $final_data = json_encode($array_data);
+     if(file_put_contents('results.json', $final_data))
+     {
+
+     }
+     else{
+         echo "Kan je me even een bericht sturen? (Jasper)" . "<br>";
+     }
+
+
+
+     $tekst = "Bedankt maat!";
+ }else{
+   $tekst = "Zou jet het even snel kunnen fixen vriend";
+ }
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>KerstOntbijt</title>
+        <link rel="stylesheet" href="styles/main.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head> 
+    <body>
+
+        <?php echo $tekst;?>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
+</html>
